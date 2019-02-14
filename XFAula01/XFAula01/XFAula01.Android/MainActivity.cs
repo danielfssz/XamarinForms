c249c43f6dcfa8f9415593766543a2b5f10f256c
Daniel Souza <danielfss98@gmail.com>
2019-02-13 22:02:14 -0200

    Add Aula01

---
+++ XFAula01/XFAula01/XFAula01.Android/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+
+namespace XFAula01.Droid
+{
+    [Activity(Label = "XFAula01", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ XFAula01/XFAula01/XFAula01/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace XFAula01
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new ContentPage
+            {
+                Content = new Label
+                {
+                    Text = "Hello World Xamarim Forms0",
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand
+                },
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
